505cfdd6d41b40cb2c794e5a96cb5e8355274382
l2010ft <7leonardo.tf4t@gmail.com>
2026-01-23 14:59:09 -0600

    modified:   loader.cpp

--- loader.cpp
+++ loader.cpp
@@ -1,2 +1,18 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+
+bool Vulkancheck();
+
+
+int main(){
+    if (Vulkancheck()){
+        
+    }else{
+
+    }
+}
+bool Vulkancheck(){
+    VkInstance instance;
+    
+    
+}
