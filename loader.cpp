52d722ce3c8277356763ec601450bc59ac9b2940
l2010ft <7leonardo.tf4t@gmail.com>
2026-01-23 15:57:31 -0600

    0.1%

--- loader.cpp
+++ loader.cpp
@@ -1,18 +1,36 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+#include <core/Engine.h>
 
 bool Vulkancheck();
 
 
 int main(){
+    engine E;
     if (Vulkancheck()){
-        
+        E.engineRun(0);
     }else{
-
+        E.engineRun(1);
     }
 }
 bool Vulkancheck(){
     VkInstance instance;
     
-    
+    VkApplicationInfo appinfo{};
+    appinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    appinfo.pApplicationName = "Engine";
+    appinfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createinfo{};
+    createinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createinfo.pApplicationInfo = &appinfo;
+
+    VkResult resuslt = vkCreateInstance(&createinfo,nullptr,&instance);
+
+    if (resuslt != VK_SUCCESS){
+        return false;
+    }
+
+    vkDestroyInstance(instance,nullptr);
+    return true;
 }
