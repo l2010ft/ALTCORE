b98e8f2dc7d8434288d96b335af8612fca1841a1
l2010ft <7leonardo.tf4t@gmail.com>
2026-03-09 21:09:09 -0600

    modified:   include/core/scriptloader/API/apipy.h 	modified:   include/core/scriptloader/scripload.cpp

---
+++ include/core/scriptloader/API/apipy.h
@@ -0,0 +1,14 @@
+#include "scriptloader/scripload.h"
+
+class EngineAPY
+{
+private:
+    make* ballistic = nullptr;
+public:
+    //inicializar antes que el constructor el puntero del ballistic :3
+    EngineAPY(make* ptr);
+    ~EngineAPY();
+};
+
+
+
--- include/core/scriptloader/scripload.cpp
+++ include/core/scriptloader/scripload.cpp
@@ -3,6 +3,6 @@
 
 extern make* maker = nullptr;
 
-make::make(std::queue<resp1>& q,std::queue<resp1>& re2){
+make::make(std::queue<resp1>& input1,std::queue<resp1>& output1,std::condition_variable& cv1,std::mutex minput,std::mutex moutput){
     
 }
